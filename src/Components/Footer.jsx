090e38f0f1315223cb0c968905eb0686560b8063
Haywhy217 <olanipekunayomide217@gmail.com>
2025-01-03 03:05:07 +0100

    edited the footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import logo from "../assets/Images/Brandlogo";
 
 const Footer = () => {
   return (
     <div className={styles.footerCTN}>
       <div className={styles.side1}>
-        <h2>PROJECTS</h2>
-        <h2>ABOUT</h2>
-        <h2>CONTACT</h2>
+        <img src={logo} className={styles.footlogo}/>
+        <p>A full-stack developer with a focus on crafting intuitive and visually appealing frontends for websites and web applications, driving the success of the overall product.</p>
       </div>
       <div className={styles.side2}>
-        <p>olanipekunayomide217@gmail.com</p>
-        <p>09058405456, 08063417132</p>
+        <h1>SOCIAL</h1>
+        <a href='https://twitter.com' target='blank' rel='noopener noreferrer'>
+        <FontAwesomeIcon icon={faTwitter} className={styles.icon2}/>
+        </a>
+        <a href='https://www.linkedin.com' target='blank' rel='noopener noreferrer'>
+        <FontAwesomeIcon icon={faLinkedin} className={styles.icon2}/>
+        </a>
+        <a href='https://github.com/Haywhy217' target='blank' rel='noopener noreferrer'>
+        <FontAwesomeIcon icon={faGithub} className={styles.icon2}/>
+        </a>
       </div>
+      <hr />
+    <p>
+      &copy; 2025. Made by <a href="/" className={styles.copyrightlink}>Olanipekun Ayomide</a>
+    </p>
     </div>
   )
 }
